d34e3fc2c11d93f49ac83c61a98239c6e0e9e1d0
Allie Robinson <allierob93@gmail.com>
2019-08-29 15:49:52 -0500

    Data from API is appearing in PhotoCard component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 import "./App.css";
+import PhotoCard from './components/PhotoCard';
 
 function App() {
+  const [data, setData] = useState({}); 
+
+  axios.get("https://api.nasa.gov/planetary/apod?api_key=f9e9i1Jhoy431QKH984SYBsYDdpA14D9wkfNK1a5")
+  .then(response => {
+    console.log(response.data);
+    setData(response.data);
+  })
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <PhotoCard title={data.title} 
+                 url={data.url}
+                 explanation={data.explanation} />
     </div>
   );
 }
